b15e9b43526b843b85875ce05ab3a58bccae5e45
Alejandro Tomasella <aletomasella09@gmail.com>
2023-04-23 23:51:09 -0300

    feat: Connecting to OPEN AI API

--- src/routes/baseRoute.ts
+++ src/routes/baseRoute.ts
@@ -1,17 +1,33 @@
 import { Router } from "express";
+import { openai } from "../config/openai.config";
 
 const baseRoute = Router();
 
-baseRoute.get("/", (req, res) => {
-  const { prompt } = req.query;
-  if (!prompt)
-    return res.status(400).send({
-      message: "Please provide a prompt",
+baseRoute.get("/", async (req, res) => {
+  try {
+    const { prompt } = req.query;
+    if (!prompt)
+      return res.status(400).send({
+        message: "Please provide a prompt",
+      });
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt.toString(),
+      max_tokens: 5,
+      temperature: 0.9,
     });
 
-  return res.status(200).send({
-    message: `You sent ${prompt}`,
-  });
+    return res.status(200).send({
+      message: `You sent ${prompt}`,
+      response,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Something went wrong",
+      error,
+    });
+  }
 });
 
 export default baseRoute;
